9a823a2fbb68a9bd214383e0eff0b38d32f8351c
Enzo Vidal <exirion@gmail.com>
2013-12-12 09:50:51 -0300

    Primeros pasos

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Post producción - Temas de Wordpress</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<div id="contenedor">
+		<div id="cabecera">
+			Esta es la cabecera
+		</div>
+		<div id="contenido">
+			Esto es el contenido
+		</div>
+		<div id="pie">
+			Esto es el pie de página
+		</div>
+	</div>
+</body>
+</html>
