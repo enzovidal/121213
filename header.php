296861de57d17fc9ecb6479044fc452c4b162e78
Enzo Vidal <exirion@gmail.com>
2013-12-12 10:33:43 -0300

    Con menú en la cabecera

--- header.php
+++ header.php
@@ -7,5 +7,12 @@
 <body>
 	<div id="contenedor">
 		<div id="cabecera">
-			Esta es la cabecera
+			<div class="nav">
+			<ul>
+				<li><a href="index.php">Inicio</a></li>
+				<li><a href="nosotros.php">Nosotros</a></li>
+				<li><a href="servicios.php">Servicios</a></li>
+				<li><a href="contacto.php">Contacto</a></li>
+			</ul>
+			</div>
 		</div>
